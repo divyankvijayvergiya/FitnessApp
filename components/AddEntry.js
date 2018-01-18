3ca4f36a715a88f48788fcd8447594a34b0eebd6
Divyank Vijayvergiya <divyankvijay.96@gmail.com>
2018-01-18 23:00:45 +0530

    Async storage implemented

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
-import { getMetricMetaInfo } from '../utils/helpers'
+import { View, TouchableOpacity, Text } from 'react-native'
+import { getMetricMetaInfo, timeToString } from '../utils/helpers'
 import UdaciSlider from './UdaciSlider'
 import UdaciSteppers from './UdaciSteppers'
+import DateHeader from './DateHeader'
+import { Ionicons } from '@expo/vector-icons'
+import TextButton from './TextButton'
+import { submitEntry, removeEntry } from '../utils/api'
+
+function SubmitBtn ({ onPress }){
+  return(
+    <TouchableOpacity
+      onPress={onPress}>
+      <Text>SUBMIT</Text>
+
+    </TouchableOpacity>
+  )
+}
 
 export default class AddEntry extends Component{
   state = {
@@ -44,10 +58,55 @@ export default class AddEntry extends Component{
     }))
   }
 
+  submit = ()=>{
+    const key = timeToString()
+    const entry = this.state
+    // Update Redux
+
+    this.setState(()=>({
+      run: 0,
+      bike: 0,
+      swim: 0,
+      sleep: 0,
+      eat: 0,
+
+    }))
+
+
+    //Navigate to home
+
+
+    submitEntry({ key, entry })
+
+    //clear local notifications
+  }
+  reset = ()=>{
+    const key = timeToString()
+
+    //Update Redux
+    //Route to home
+    removeEntry(key)
+  }
+
   render(){
     const metaInfo =getMetricMetaInfo()
+    if(this.props.alreadyLogged){
+      return(
+        <View>
+          <Ionicons
+            name='ios-happy-outline'
+            size={100}
+          />
+          <Text>You already logged your information for today</Text>
+          <TextButton onPress={this.reset}>
+            Reset
+          </TextButton>
+        </View>
+      )
+    }
     return(
       <View>
+        <DateHeader date={(new Date()).toLocaleDateString()}/>
         {Object.keys(metaInfo).map((key)=>{
           const { getIcon, type, ...rest } = metaInfo[key]
           const value = this.state[key]
@@ -63,13 +122,14 @@ export default class AddEntry extends Component{
                 : <UdaciSteppers
                       value={value}
                       onIncrement={()=> this.increment(key)}
-                      onDncrement={()=> this.decrement(key)}
+                      onDecrement={()=> this.decrement(key)}
                       {...rest}
                   />
                 }
             </View>
           )
         })}
+        <SubmitBtn onPress={this.submit}/>
       </View>
     )
   }
